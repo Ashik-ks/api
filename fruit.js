652918a3d49baf0a77c514da7de759091ab74ff6
ashik <ashik123ks@gmail.com>
2024-08-17 19:13:44 +0530

    done

--- fruit.js
+++ fruit.js
@@ -28,11 +28,21 @@
 
                     let carouselid = document.getElementById('carouselid');
                     console.log ("carouselid",carouselid);
+
                     let datacontainer1 = document.getElementById('datacontainerdiv1');
                     console.log ("datacontainer1",datacontainer1)
 
-                    let rows = ''
-                    let rows1 = ''
+                    let datacontainer2 = document.getElementById('datacontainerdiv2');
+                    console.log ("datacontainer2",datacontainer2)
+
+                    let datacontainerdiv3 = document.getElementById('datacontainerdiv3datacontainerdiv3');
+                    console.log ("datacontainerdiv3",datacontainerdiv3)
+
+
+                    let rows = '';
+                    let rows1 = '';
+                    let rows2 = '';
+                    let rows3 = '';
 
                     for (i=0 ; i < datas.length-16 ; i++){
                         rows = rows + `
@@ -67,15 +77,43 @@
                       rows1 = rows1 + `
                       
                      <div id = "datacontainer1" class ="shadow p-3 mb-5 bg-body rounded lh-lg">
-                     <div id = "imageid2" class="text-center" ><img src ="${datas[i].image} "class = "carouselimg1"></div>
-                      <div id = "titleid2" class = "mt-5">${datas[i].title}</div>
-                        <div id = "categoryid2">${datas[i].category}</div>
-                        <div id = "ratingid2">Rating : ${datas[i].rating.rate}</div>
+                     <div id = "imageid1" class="text-center" ><img src ="${datas[i].image} "class = "carouselimg1"></div>
+                     <div id = "titleid1" class = "mt-5">${datas[i].title}</div>
+                     <div id = "categoryid1">${datas[i].category}</div>
+                     <div id = "ratingid1">Rating : ${datas[i].rating.rate}</div>
                      </div>
                       
                       
                       `
                     }
+
+                    for (i = 8; i < datas.length - 8; i++){
+                        rows2 = rows2 + `
+                        
+                       <div id = "datacontainer2" class ="shadow p-3 mb-5 bg-body rounded lh-lg">
+                       <div id = "imageid2" class="text-center" ><img src ="${datas[i].image} "class = "carouselimg1"></div>
+                        <div id = "titleid2" class = "mt-5">${datas[i].title}</div>
+                          <div id = "categoryid2">${datas[i].category}</div>
+                          <div id = "ratingid2">Rating : ${datas[i].rating.rate}</div>
+                       </div>
+                        
+                        
+                        `
+                      }
+
+                      for (i = 12; i < datas.length - 4; i++){
+                        rows3 = rows3 + `
+                        
+                       <div id = "datacontainer2" class ="shadow p-3 mb-5 bg-body rounded lh-lg">
+                       <div id = "imageid2" class="text-center" ><img src ="${datas[i].image} "class = "carouselimg1"></div>
+                        <div id = "titleid2" class = "mt-5">${datas[i].title}</div>
+                          <div id = "categoryid2">${datas[i].category}</div>
+                          <div id = "ratingid2">Rating : ${datas[i].rating.rate}</div>
+                       </div>
+                        
+                        
+                        `
+                      }
 
 
 
@@ -83,6 +121,8 @@
                      
                     carouselid.innerHTML = rows;
                     datacontainer1.innerHTML = rows1;
+                    datacontainer2.innerHTML = rows2;
+                    datacontainer3.innerHTML = rows3;
 
 
                    
