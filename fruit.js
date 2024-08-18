5e90b62fd2b3fc37a258e194e8ee274143316b54
ashik <ashik123ks@gmail.com>
2024-08-18 21:22:26 +0530

    all done

--- fruit.js
+++ fruit.js
@@ -1,6 +1,6 @@
 {
 
-    window.onload = function loaddata() {
+    function loaddata() {
         let xhr = new XMLHttpRequest();
         console.log("xhr : ", xhr);
 
@@ -26,17 +26,17 @@
                     console.log("datas : ", datas);
                     console.log(typeof (datas))
 
-                    let carouselid = document.getElementById('carouselid');
+                    let carouselid = document.getElementById('carouselidd');
                     console.log ("carouselid",carouselid);
 
                     let datacontainer1 = document.getElementById('datacontainerdiv1');
-                    console.log ("datacontainer1",datacontainer1)
+                    console.log ("datacontainer1",datacontainer1);
 
                     let datacontainer2 = document.getElementById('datacontainerdiv2');
                     console.log ("datacontainer2",datacontainer2)
 
-                    let datacontainerdiv3 = document.getElementById('datacontainerdiv3datacontainerdiv3');
-                    console.log ("datacontainerdiv3",datacontainerdiv3)
+                    let datacontainer3 = document.getElementById('datacontainerdiv3');
+                    console.log ("datacontainerdiv3",datacontainer3)
 
 
                     let rows = '';
@@ -44,97 +44,166 @@
                     let rows2 = '';
                     let rows3 = '';
 
-                    for (i=0 ; i < datas.length-16 ; i++){
-                        rows = rows + `
-                       
-                        
-                       
-                        <div class="carousel-item ${i === 0 ? 'active' : ''} ">
-                        <div class="d-flex lh-md">
-                        <div class="div1">
-                        <div id = "titleid">${datas[i].title}</div>
-                        <div id = "descriptionid">${datas[i].description.slice(0,150)+"..."}</div>
-                        <div id = "categoryid">${datas[i].category}</div>
-                        <div id = "ratingid">Rating : ${datas[i].rating.rate}</div>
-                        </div>
-                        <div class="div2">
-                        <div id = "imageid" ><img src ="${datas[i].image} "class = "carouselimg"></div>
-                        
-                        
-                        </div>
-                        </div>
-                        </div>
-                        
-                        
-                     `
-                     
-                        
-                         
-                    }
-
+                    let arr1=["men's clothing","jewelery","electronics","women's clothing"]
                     
-                    for (i = 4; i < datas.length - 12; i++){
-                      rows1 = rows1 + `
-                      
-                     <div id = "datacontainer1" class ="shadow p-3 mb-5 bg-body rounded lh-lg">
+                    for(i=0 ; i < datas.length ; i++){
+                      for(j=0; j< arr1.length-3;j++){
+                        if(datas[i].category === arr1[0]){
+                          rows = rows + `
+                          <div class="carousel-item ${i === 0 ? 'active' : ''} ">
+                          <div class="d-flex lh-md">
+                          <div class="div1">
+                          <div id = "titleid">${datas[i].title}</div>
+                          <div id = "descriptionid">${datas[i].description.slice(0,150)+"..."}</div>
+                          <div id = "categoryid">${datas[i].category}</div>
+                          <div id = "ratingid">Rating : ${datas[i].rating.rate}</div>
+                          <div id="btnid" class = "text-center"><button onclick="handleClick(${datas[i].id})">Details</button></div>
+                          </div>
+                          <div class="div2">
+                          <div id = "imageid" ><img src ="${datas[i].image} "class = "carouselimg"></div>
+                     
+                          </div>
+                          </div>
+                          </div>
+                          `
+                        }
+                        
+                        if(datas[i].category === arr1[1]){
+                          rows1 = rows1 + `
+                          <div id = "datacontainer1" class ="shadow p-3 mb-5 bg-body rounded lh-lg">
                      <div id = "imageid1" class="text-center" ><img src ="${datas[i].image} "class = "carouselimg1"></div>
                      <div id = "titleid1" class = "mt-5">${datas[i].title}</div>
                      <div id = "categoryid1">${datas[i].category}</div>
                      <div id = "ratingid1">Rating : ${datas[i].rating.rate}</div>
+                     <div id="btnid" class = "text-center"><button onclick="handleClick(${datas[i].id})">Details</button></div>
                      </div>
+                          `
+                        }
+
+                        if(datas[i].category === arr1[2]){
+                          rows2 = rows2 + `
+                          <div id = "datacontainer2" class ="shadow p-3 mb-5 bg-body rounded lh-lg">
+                       <div id = "imageid2" class="text-center" ><img src ="${datas[i].image} "class = "carouselimg1"></div>
+                        <div id = "titleid2" class = "mt-5">${datas[i].title}</div>
+                          <div id = "categoryid2">${datas[i].category}</div>
+                          <div id = "ratingid2">Rating : ${datas[i].rating.rate}</div>
+                         <div id="btnid" class = "text-center"><button onclick="handleClick(${datas[i].id})">Details</button></div>
+                       </div>
+                       `
+                        }
+
+                        if(datas[i].category === arr1[3]){
+                          rows3 = rows3 + `
+                          <div id = "datacontainer2" class ="shadow p-3 mb-5 bg-body rounded lh-lg">
+                       <div id = "imageid2" class="text-center" ><img src ="${datas[i].image} "class = "carouselimg1"></div>
+                        <div id = "titleid2" class = "mt-5">${datas[i].title}</div>
+                          <div id = "categoryid2">${datas[i].category}</div>
+                          <div id = "ratingid2">Rating : ${datas[i].rating.rate}</div>
+                         <div id="btnid" class = "text-center"><button onclick="handleClick(${datas[i].id})">Details</button></div>
+                          
+                       </div>
+                       `
+                        }
+                        carouselid.innerHTML = rows;
+                        console.log("rows : ",rows);
+                        datacontainer1.innerHTML = rows1;
+                        datacontainer2.innerHTML = rows2;
+                        datacontainer3.innerHTML = rows3;
+                      }
                       
-                      
-                      `
+
                     }
-
-                    for (i = 8; i < datas.length - 8; i++){
-                        rows2 = rows2 + `
-                        
-                       <div id = "datacontainer2" class ="shadow p-3 mb-5 bg-body rounded lh-lg">
-                       <div id = "imageid2" class="text-center" ><img src ="${datas[i].image} "class = "carouselimg1"></div>
-                        <div id = "titleid2" class = "mt-5">${datas[i].title}</div>
-                          <div id = "categoryid2">${datas[i].category}</div>
-                          <div id = "ratingid2">Rating : ${datas[i].rating.rate}</div>
-                       </div>
-                        
-                        
-                        `
-                      }
-
-                      for (i = 12; i < datas.length - 4; i++){
-                        rows3 = rows3 + `
-                        
-                       <div id = "datacontainer2" class ="shadow p-3 mb-5 bg-body rounded lh-lg">
-                       <div id = "imageid2" class="text-center" ><img src ="${datas[i].image} "class = "carouselimg1"></div>
-                        <div id = "titleid2" class = "mt-5">${datas[i].title}</div>
-                          <div id = "categoryid2">${datas[i].category}</div>
-                          <div id = "ratingid2">Rating : ${datas[i].rating.rate}</div>
-                       </div>
-                        
-                        
-                        `
-                      }
-
-
-
-                    console.log(rows);
-                     
-                    carouselid.innerHTML = rows;
-                    datacontainer1.innerHTML = rows1;
-                    datacontainer2.innerHTML = rows2;
-                    datacontainer3.innerHTML = rows3;
-
-
-                   
-
+                    
                 }
-
             }
         }
-
     }
 
-
-
+      
+    function  handleClick(id){
+      console.log("button clicked");
+      console.log("button clicked id :",id)
+      window.location.href =`view.html?id=${id}`
+      return;
+    }
+    
+    
+    
+    function loadedproducts() {
+      console.log("loading....")
+    
+        let location = window.location;
+        console.log("location", location);
+    
+        let querystring = location.search;
+        console.log("querystring", querystring);
+    
+    
+        let urlParams = new URLSearchParams(querystring);
+        console.log("url", urlParams);
+    
+        let id = urlParams.get("id");
+        console.log("id ", id);
+    
+        let xhr = new XMLHttpRequest();
+    
+        xhr.open("get", `https://fakestoreapi.com/products/${id} `)
+    
+        xhr.send();
+    
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+              console.log("status:", xhr.status);
+    
+              if (xhr.status === 200) {
+                  console.log("success");
+    
+                  let userData = xhr.response;
+                  console.log("userData:",userData);
+    
+                  let parsed_userData = JSON.parse (userData);
+                  console.log("parsed_userData",parsed_userData);
+    
+    
+                  let load = document.getElementById('loadeditems');
+                  console.log("load",load);
+                  let load1 = document.getElementById('loadeditems1');
+                  let load2 = document.getElementById('loadeditems2');
+                  let load3 = document.getElementById('loadeditems3');
+                  let load4 = document.getElementById('loadeditems4');
+                  // let load5 = document.getElementById('loadeditems1');
+                  // let load6 = document.getElementById('loadeditems1');
+                  
+    
+    
+                  // load.value =.title;
+                  
+                      load.innerHTML = parsed_userData.title; 
+                      load1.innerHTML = parsed_userData.description; 
+                      // load2.innerHTML =<img src="${parsed_userData.image}" style="width:400px; height:400px;">
+                      // load3.innerHTML ="{parsed_userData.image}"
+    
+                  
+                      console.log("Element with id 'loadeditems' not found");
+                  
+    
+              }else{
+                  alert("failed to load")
+              }
+              
+              
+           }
+        }
+    }
 
 }
+  
+  
+
+
+
+
+
+
+   
+                     
